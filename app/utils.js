116d65ccf5738d961e9b505df991a73113f5f13f
Valeriy.Vyrva <valery1707@gmail.com>
2016-01-18 20:33:55 +0600

    Use decimal count in formatBytes

--- app/utils.js
+++ app/utils.js
@@ -11,9 +11,9 @@ function formatBytes(bytes, decimals, k, sizes) {
 	if (bytes == 0) {
 		return '0 Byte';
 	}
-	var dm = decimals + 1 || 3;
+	var dm = decimals + 0 || 2;
 	var i = Math.floor(Math.log(bytes) / Math.log(k));
-	return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+	return (bytes / Math.pow(k, i)).toFixed(dm) + ' ' + sizes[i];
 }
 /**
  * Format bytes to string in 1024 mode
